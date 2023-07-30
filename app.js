0c80a14e63ece19e91d9cdb42f7535bc1f84a08e
Divyanshi Agrawal <92880987+divyanshi128@users.noreply.github.com>
2023-07-30 17:47:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+
+const input= document.querySelector('input');
+const buttons=document.querySelectorAll('button');
+const click= new Audio('click-button.mp3');
+
+
+let arr=[];
+for(let button of buttons){
+    button.addEventListener('click',function(event){
+        click.play();
+        let button_value= event.target.innerText
+        arr.push(`${button_value}`)
+        // console.log(arr)
+        if(button_value==='C'){
+            input.value= '';
+        }
+        else if(button_value===''){
+            let len=input.value.length;
+            input.value= input.value.substring(0, len-1);
+                
+        }
+
+        else if(button_value==='='){
+            try{
+                input.value=input.value.replaceAll('×','*')
+                input.value= eval(input.value);
+            }
+            catch(e){
+                input.value= 'Error!!';
+            }
+        }
+        else{
+            let currIndex=arr.indexOf(`${button_value}`)
+            if(arr[currIndex-1]!=='='){
+                input.value+=button_value;
+            }
+                
+            
+            else{
+                input.value=button_value
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
